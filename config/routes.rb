19e737ebf13cd7436259cbfff1d1f6cf585b5cf5
adam <amartenj@gmail.com>
2017-07-14 14:16:17 -0700

    User and session ajax actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/root'
-
   root "static_pages#root"
+
+  namespace :api, defaults: { format: :json } do
+    resources :sessions, only: [:create, :destroy]
+    resources :users, only: [:create]
+  end
+
 end
