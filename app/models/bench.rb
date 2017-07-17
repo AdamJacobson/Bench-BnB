706f3175d0b764c29b0cf0c228a36334be23b43f
adam <amartenj@gmail.com>
2017-07-17 15:30:57 -0700

    Add model level validations to Bench

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -1,2 +1,9 @@
 class Bench < ApplicationRecord
+  validates :description, presence: true
+  validates :lat, inclusion: {
+    in: -90..90,
+    message: "must be between -90 and 90 inclusive" }
+  validates :lon, inclusion: {
+    in: -180..180,
+    message: "must be between -180 and 180 inclusive" }
 end
